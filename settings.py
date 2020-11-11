80c5cefc9e2f3be1e82664c2e6f68a36a7305bc4
bbbnnn7 <nbbbyambaa@gmail.com>
2020-11-11 22:41:18 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,23 @@
+import random
+
+class Settings:
+	def __init__(self):
+		self.width = 800
+		self.height = 900
+		self.bg_color = (250 , 250 , 250)
+		self.bb_color = (150  , 150 , 150)
+		self.bb_width = 600
+		self.bb_height = 600
+		self.num_color = []
+		self.change_color()
+
+	def change_color(self):
+		color = (0 , 0 , 0)
+		for i in range(3):
+			for j in range(5):
+				neco = list(color)
+				neco[i] += (j + 1) * 50
+				neco = tuple(neco)
+				self.num_color.append(neco)
+		self.num_color[0] = (200 , 200 , 200)
+		self.num_color.insert(0 , (100 , 100 , 100))
