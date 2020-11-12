8eb7ba6c5f5df0b6fadd5b24ebfb8f49ebc113b0
bbbnnn7 <43579379+bbbnnn7@users.noreply.github.com>
2020-11-12 14:55:44 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,5 +19,5 @@ class Settings:
 				neco[i] += (j + 1) * 50
 				neco = tuple(neco)
 				self.num_color.append(neco)
-		self.num_color[0] = (200 , 200 , 200)
-		self.num_color.insert(0 , (100 , 100 , 100))
+		self.num_color[0] = (150 , 150 , 150)
+		self.num_color.insert(0 , (200 , 200 , 200))
